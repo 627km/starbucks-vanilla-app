46ab075d993fa7dd9885034cbf01bf2ca955983b
627km <rudals_jeon@naver.com>
2024-02-09 22:38:53 +0900

    feat: Make Badges

--- js/main.js
+++ js/main.js
@@ -2,15 +2,40 @@ const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
 searchEl.addEventListener('click', () => {
-    searchInputEl.focus();
+  searchInputEl.focus();
 });
 
 searchInputEl.addEventListener('focus', () => {
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '통합검색');
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
 });
 
 searchInputEl.addEventListener('blur', () => {
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener(
+  'scroll',
+  _.throttle(() => {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+      // 배지 숨기기
+      // gsap.to(요소, 지속시간, 옵션);
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: 'none',
+      });
+    } else {
+      // 배지 보여주기
+      badgeEl.style.display = 'block';
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: 'block',
+      });
+    }
+  }, 300)
+);
+// _.throttle(함수, 시간)
